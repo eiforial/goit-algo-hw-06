7c36dd4858ce48c5857a36cb4b482671d3f67400
eiforial <rabudal743@gmail.com>
2024-04-01 12:47:05 +0200

    HW-1.py

---
+++ HW-1.py
@@ -0,0 +1,125 @@
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    pass
+
+class Phone(Field):
+    pass
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def edit_phone(self, old_phone, new_phone):
+        for phone in self.phones:
+            if phone.value == old_phone:
+                phone.value = new_phone
+
+    def find_phone(self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                return phone.value
+        return None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except KeyError:
+            return "Contact not found."
+        except IndexError:
+            return "Invalid command format. Use: [command] [name] [phone]"
+
+    return inner
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, args
+
+@input_error
+def add_contact(args, contacts):
+
+    if len(args) == 2:
+        name, phone = args
+        contacts[name] = phone
+        return "Contact added."
+
+@input_error
+def change_contact(args, contacts):
+
+    if len(args) == 2:
+        name, phone = args
+        if name in contacts:
+            contacts[name] = phone
+            return "Contact updated."
+
+@input_error
+def show_phone(args, contacts):
+
+    if len(args) == 1:
+        name = args[0]
+        if name in contacts:
+            return contacts[name]
+    
+@input_error
+def show_all(contacts):
+
+    if contacts:
+        return "\n".join([f"{name}: {phone}" for name, phone in contacts.items()])
+
+def main():
+
+    contacts = {}
+
+    print("Welcome to the assistant bot!")
+
+    while True:
+        user_input = input("Enter a command: ").strip().lower()
+        command, args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "phone":
+            print(show_phone(args, contacts))
+        elif command == "all":
+            print(show_all(contacts))
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
